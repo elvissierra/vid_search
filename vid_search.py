b4e4ceb69dc6bb8ff770c0171733ff36868e919e
elvissierra <elvissierra3@gmail.com>
2024-10-28 15:30:44 -0500

    feat: rewrite transcription script using Whisper and psycopg2

--- vid_search.py
+++ vid_search.py
@@ -1,35 +1,30 @@
-import moviepy.editor as mp
-import speech_recognition as sr
+import psycopg2
+import environ
 import whisper
 
-"""video = mp.VideoFileClip("what.mp4")
-audio_file = video.audio
-audio_file.write_audiofile("what.wav")
-r = sr.Recognizer()
-chunk_duration = 2
-with sr.AudioFile("what.wav") as source:
-    total_duration = source.DURATION
-    offset = 0
-    while offset < total_duration:
-        data = r.record(source, duration=total_duration, offset=offset)
-        try:
-            results = r.recognize_google(data, show_all=True)
-            if isinstance(results, dict):
-                for result in results["alternative"]:
-                    if "confidence" in result:
-                        print(f"Text: {result['transcript']} (Start: {offset} seconds, End: {offset + chunk_duration} seconds, Confidence: {result['confidence']})")
-            else:
-                print(f"Speech not recognized at: {offset} to {offset + chunk_duration}")
+env = environ.Env()
+environ.Env.read_env()
 
-        except sr.UnknownValueError:
-            print(f"Speech not recognized at: {offset} to {offset + chunk_duration}")
-        offset += chunk_duration"""
-#use whisper to identify text and its timestamp for search functionality
-model = whisper.load_model("base")
-result = model.transcribe("videos/msg.mp4", word_timestamps=True)
+with psycopg2.connet(
+    dbname=env("DA_NAME"), user=env("DB_USER"), password=env("DB_PASSWORD"), host=env("DB_HOST")
+    )as conn:
+        with conn.cursor()as cursor:
 
-for segment in result['segments']:
-    print(f"Segment: {segment['text']}, Start: {segment['start']}, End: {segment['end']}")
-    if 'words' in segment:
-        for word in segment['words']:
-            print(f"  Word: {word['word']}, Start: {word['start']}, End: {word['end']}")
+            model = whisper.load_model("base")
+            result = model.transcribe("videos/msg.mp4", word_timestamps=True)
+
+            for segment in result["segments"]:
+                segment_text = segment['text']
+                for word in segment['words']:
+                    start_time = word['start']
+                    end_time = word['end']
+                    word_text = word['word']
+
+                    cursor.execute(
+                        "INSERT INTO transcripts (video_id, segment_text, word, start_time, end_time) VALUES (%s, %s, %s, %s, %s)",
+                        (1, segment_text, word_text, start_time, end_time)
+                    )
+
+            conn.commit()
+            cursor.close()
+            conn.close()
