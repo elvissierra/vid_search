5ff49137a902e9ca94982f26944d8d11ba5efed0
elvissierra <elvissierra3@gmail.com>
2024-10-31 11:57:30 -0500

    keyword query

--- vid_search.py
+++ vid_search.py
@@ -2,6 +2,7 @@ import psycopg2
 import environ
 import whisper
 import yt_dlp
+import uuid
 import numpy as np
 
 env = environ.Env()
@@ -34,41 +35,30 @@ def transcribe_save(url):
     model = whisper.load_model("base")
     result = model.transcribe(audio_file, word_timestamps=True)
 
+    video_id = str(uuid.uuid4())
+
     with psycopg2.connect(
         dbname=env("DB_NAME"), user=env("DB_USER"), password=env("DB_PASSWORD"), host=env("DB_HOST")
-        ) as conn:
-            with conn.cursor() as cursor:
+    ) as conn:
+        with conn.cursor() as cursor:
+            for segment in result["segments"]:
+                segment_text = str(segment['text'])
+                for word in segment['words']:
+                    word_text = str(word['word'])
+                    start_time = float(word['start'])
+                    end_time = float(word['end'])
+                    
+                    cursor.execute(
+                        """
+                        INSERT INTO transcripts (video_id, segment_text, word, start_time, end_time)
+                        VALUES (%s, %s, %s, %s, %s);
+                        """,
+                        (video_id, segment_text, word_text, start_time, end_time)
+                    )
 
-                cursor.execute(
-                    """
-                    INSERT INTO transcripts (segment_text, word, start_time, end_time)
-                    VALUES (%s, %s, %s, %s)
-                    RETURNING video_id;
-                    """,
-                    (str(result['segments'][0]['text']),
-                    str(result['segments'][0]['words'][0]['word']), 
-                    float(result['segments'][0]['words'][0]['start']),
-                    float(result['segments'][0]['words'][0]['end']))
-                )
-                video_id = cursor.fetchone()[0]
+            conn.commit()
 
-                for segment in result["segments"]:
-                    segment_text = str(segment['text'])
-                    for word in segment['words']:
-                        word_text = str(word['word'])
-                        start_time = float(word['start'])
-                        end_time = float(word['end'])     
-
-                        cursor.execute(
-                            """
-                            INSERT INTO transcripts (video_id, segment_text, word, start_time, end_time)
-                            VALUES (%s, %s, %s, %s, %s);
-                            """,
-                            (video_id, segment_text, word_text, start_time, end_time)
-                        )
-                conn.commit()
-
-            return video_id
+    return video_id
     
 if __name__ == "__main__":
     video_url = 'https://www.youtube.com/watch?v=5GzFhlfxGxc&ab_channel=HouseofHighlights'
